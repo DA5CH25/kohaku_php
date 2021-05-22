d8f520ce5335bc276cc7d23a862637bcd8e5157f
Bunker1000 <73027527+Bunker1000@users.noreply.github.com>
2021-05-22 12:43:27 -0500

    cambio crear usuario
    crear usuario

---
+++ .history/models/Roles.dao_20210522111836.php
@@ -0,0 +1,37 @@
+<?php
+require_once './models/Conexion.clase.php';
+require_once './models/Rol.clase.php'; 
+
+class RolesDAO {
+	public static function listarDatos () {
+		$con = new Conexion();
+		$cont = $con->ejecutarConsulta('SELECT * FROM usuario');
+		$con->cerrarConexion();
+		return $cont;
+	}
+
+	public static function ingresarDato ($rol) {
+		$con = new Conexion();
+		$con->ejecutarActualizacion("INSERT INTO usuario (nombre, apellido, numero_identificacion, telefono_fijo, telefono_movil, direccion,correo_electronico, clave ) VALUES ('$rol->nombre','$rol->apellido','$rol->numero_identificacion','$rol->telefono_fijo','')");
+		$con->cerrarConexion();
+	}
+
+	public static function buscarPorId ($id) {
+		$con = new Conexion();
+		$cont = $con->ejecutarConsulta("SELECT * FROM usuario WHERE id_usuario = $id_usuario");
+		$con->cerrarConexion();
+		return $cont[0];
+	}
+
+	public static function editarDato ($rol) {
+		$con = new Conexion();
+		$con->ejecutarActualizacion("UPDATE usuario SET Nombre = '$rol->nombre' WHERE id_usuario = $rol->id_usuario");
+		$con->cerrarConexion();
+	}
+
+	public static function eliminarPorId ($id) {
+		$con = new Conexion();
+		$con->ejecutarActualizacion("DELETE FROM usuario WHERE id_usuario = $id_usuario");
+		$con->cerrarConexion();
+	}
+}
